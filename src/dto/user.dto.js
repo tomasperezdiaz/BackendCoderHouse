13a0829e460ee0c562141d6659d9e294c8122c46
Tomas Perez Diaz <tomasperezdiaz03@gmail.com>
2024-07-01 17:57:24 -0300

    AplicacionDTO

--- src/dto/user.dto.js
+++ src/dto/user.dto.js
@@ -1,9 +1,13 @@
 class UserDTO {
-    constructor(name, lastName, rol) {
-        this.name = name;
-        this.lastName = lastName;
-        this.rol = rol;
-    }
+  constructor(user) {
+    this.name = user.name;
+    this.lastName = user.lastName;
+    this.rol = user.rol;
+    this.email = user.email;
+    this.age = user.age ? user.age : "-";
+    this.cart = user.cart;
+    this.role = user.role;
+  }
 }
 
 export default UserDTO;
